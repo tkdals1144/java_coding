a767cd574fbc0803dc13ed65862116582167721e
tkdals1144 <146908349+tkdals1144@users.noreply.github.com>
2026-01-02 23:50:32 +0900

    study coding
    update

---
+++ Studying/src/Studying/B27889.java
@@ -0,0 +1,25 @@
+package Studying;
+
+import java.util.Scanner;
+
+public class B27889 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String input = sc.nextLine();
+		switch (input) {
+		case "NLCS":
+			System.out.println("North London Collegiate School");
+			break;
+		case "BHA":
+			System.out.println("Branksome Hall Asia");
+			break;
+		case "KIS":
+			System.out.println("Korea International School");
+			break;
+		case "SJA":
+			System.out.println("St. Johnsbury Academy");
+			break;
+		}
+	}
+}
