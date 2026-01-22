b58e5aeba3876f20094e0aabf964eac7c7448ad3
tkdals1144 <146908349+tkdals1144@users.noreply.github.com>
2026-01-22 22:33:35 +0900

    study coding
    update

---
+++ Studying/src/Studying/B5717.java
@@ -0,0 +1,21 @@
+package Studying;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B5717 {	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		while(true) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			if (a == 0 && b == 0) break;
+			sb.append(a+b).append('\n');
+		}
+		System.out.println(sb.toString());
+	}
+}
